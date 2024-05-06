e7ef468885848f43f6a94a5ed34c30c118a28d41
Waffle <enrico.maricar@gmail.com>
2024-05-06 16:17:26 +0700

    Initial Commit

---
+++ TestingGit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestingGit
+//
+//  Created by Enrico Maricar on 06/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestingGit/TestingGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingGitApp.swift
+//  TestingGit
+//
+//  Created by Enrico Maricar on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
